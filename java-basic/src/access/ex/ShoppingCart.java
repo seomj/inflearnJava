4b4635de918879b659543707b4058c3fc10cd86e
seomj <smj100394@gmail.com>
2025-02-14 14:07:35 +0900

    실행을 위한 주석 처리 및 코드 수정

--- java-basic/src/access/ex/ShoppingCart.java
+++ java-basic/src/access/ex/ShoppingCart.java
@@ -18,11 +18,11 @@ public class ShoppingCart {
         int totalPrice;
         System.out.println("장바구니 상품 출력");
 
-        for (Item item : items) {
+        /*for (Item item : items) {
             System.out.println("상품명: " + item.name + ", 합계: " + item.price*item.quantity);
             totalPrice += item.price*item.quantity;
         }
 
-        System.out.println("전체 가격 합: " + totalPrice);
+        System.out.println("전체 가격 합: " + totalPrice);*/
     }
 }
--- java-basic/src/access/ex/ShoppingCartRef.java
+++ java-basic/src/access/ex/ShoppingCartRef.java
@@ -1,10 +1,10 @@
 package access.ex;
 
 public class ShoppingCartRef {
-    private Item[] items = new Item[10];
+    private ItemRef[] items = new ItemRef[10];
     private int itemCount;
 
-    public void addItem(Item item){
+    public void addItem(ItemRef item){
         if (itemCount >= items.length){
             System.out.println("장바구니가 가득 찼습니다.");
             return;
@@ -18,7 +18,7 @@ public class ShoppingCartRef {
         System.out.println("장바구니 상품 출력");
 
         for (int i = 0; i < itemCount; i++) {
-            Item item = items[i];
+            ItemRef item = items[i];
             System.out.println("상품명: " + item.getName() + ", 합계: " + item.getTotalPrice());
         }
 
@@ -28,7 +28,7 @@ public class ShoppingCartRef {
     private int calculateTotalPrice(){
         int totalPrice = 0;
         for (int i = 0; i < itemCount; i++){
-            Item item = items[i];
+            ItemRef item = items[i];
             totalPrice += item.getTotalPrice();
         }
         return totalPrice;
