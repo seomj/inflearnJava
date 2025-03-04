bd9bcd8215dcb9991a83e998bf048a82b4e6c399
seomj <smj100394@gmail.com>
2025-03-04 16:33:54 +0900

    김영한의 실전 자바 - 기본편 섹션13

---
+++ java-basic/src/poly/ex/pay0/PayService.java
@@ -0,0 +1,17 @@
+package poly.ex.pay0;
+
+public class PayService {
+    public void processPay(String option, int amount) {
+        System.out.println("결제를 시작합니다: option=" + option + ", amount=" + amount);
+        
+        Payment payment = PayStore.findpay(option);
+        boolean result = payment.pay(amount);
+
+        if (result) {
+            System.out.println("결제가 성공했습니다.");
+        } else {
+            System.out.println("결제가 실패했습니다.");
+        }
+    }
+
+}
---
+++ java-basic/src/poly/ex/pay0/Payment.java
@@ -0,0 +1,5 @@
+package poly.ex.pay0;
+
+public interface Payment {
+    boolean pay(int amount);
+}
