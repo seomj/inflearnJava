f2aeaeaf7923de58eda77b0c8375680176515479
seomj <smj100394@gmail.com>
2025-10-07 16:36:39 +0900

    김영한의 실전 자바 - 중급 2편 섹션10

---
+++ java-mid2/src/collection/map/MapMain3.java
@@ -0,0 +1,21 @@
+package collection.map;
+
+import java.util.HashMap;
+
+public class MapMain3 {
+    public static void main(String[] args) {
+        HashMap<String, Integer> studentMap = new HashMap<>();
+
+        studentMap.put("studentA", 50);
+        System.out.println(studentMap);
+
+        if (!studentMap.containsKey("studentA")){
+            studentMap.put("studentA", 100);
+        }
+        System.out.println(studentMap);
+
+        studentMap.putIfAbsent("studentA", 100);
+        studentMap.putIfAbsent("studentB", 100);
+        System.out.println("studentMap = " + studentMap);
+    }
+}
